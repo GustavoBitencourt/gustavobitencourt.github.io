bf3ca74292b95f9ed3738600cdf8aa6edebcb2c2
GustavoBitencourtMe <gubitencourt8@gmail.com>
2025-10-21 19:12:31 -0300

    Add hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import StarryBackground from './components/StarryBackground';
 import LanguageSwitcher from './components/LanguageSwitcher';
 import IntroSection from './components/IntroSection';
